483316e743efd29415aa4f39603d8b4d70b651cd
perezyuan <yzmspirit@sina.cn>
2017-01-04 17:43:01 +0800

    test

--- lodash/webpack.config.js
+++ lodash/webpack.config.js
@@ -1,10 +1,18 @@
 var path = require('path'); //node自带的path模块
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 var config = {
   entry: path.join(__dirname, 'src', 'main'),
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name]-[hash].js',
   },
+  plugins: [new HtmlWebpackPlugin({
+      template: 'test.html', // 源模板文件
+      filename: 'test.html',
+      inject: true
+    }
+  )],
   module: {
     loaders: [
       {
