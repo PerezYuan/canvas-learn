12e299031c8abceccb2ed1761be28b6b6e34de1a
perezyuan <yzmspirit@sina.cn>
2017-01-09 14:19:26 +0800

    comment

--- lodash/src/_root.js
+++ lodash/src/_root.js
@@ -1,9 +1,11 @@
 import freeGlobal from './_freeGlobal.js';
 
 /** Detect free variable `self`. */
+/** 详见_freeGlobal.js中逻辑与的思考，此时判断是否为浏览器环境. */
 var freeSelf = typeof self == 'object' && self && self.Object === Object && self;
 
 /** Used as a reference to the global object. */
+/** 先判断服务端环境，再对浏览器环境判断，如都不满足返回Function存在的this */
 var root = freeGlobal || freeSelf || Function('return this')();
 
 export default root;
