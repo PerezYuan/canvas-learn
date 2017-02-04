26ad73b0f544ec02f72651940b6865c337ce3634
perezyuan <yzmspirit@sina.cn>
2017-02-04 18:33:32 +0800

    q4 update

--- question/q4.js
+++ question/q4.js
@@ -13,7 +13,21 @@ function flatten(arr) {
     var flatten_arr = arr.join(',').split(',');
     var flatten_arr2 = arr.toString().split(',');
     // 问题出现在split之后为['1','2','3']
-    return flatten_arr2;
+    return flatten_arr;
 }
 
-console.log(flatten([1,[2,3]]));
+var result = [];
+function flat(arr) {
+    // 递归方式
+    if (Array.isArray(arr)) {
+        for (var i = 0; i < arr.length; i++) {
+            flat(arr[i]);
+        }
+    } else {
+        result.push(arr);
+    }
+    return result;
+}
+
+console.log(flatten([1, [2, 3]]));
+console.log(flat([1, [2, 3]]));
