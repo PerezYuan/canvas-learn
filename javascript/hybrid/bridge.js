29948827b7711ae5e93eab4f6dc8d1d5e93138dc
perezyuan <yzmspirit@sina.cn>
2017-02-10 20:51:10 +0800

    release bridge.js

--- javascript/hybrid/bridge.js
+++ javascript/hybrid/bridge.js
@@ -1,44 +1,16 @@
-/*
-本文件包含3个功能
-
-1.bridge
-2.域名拼接
-3.查询参数获取
-
-*/
 (function(window) {
     var DEBUG = !!~window.location.href.indexOf('bridge-debug=true');
     var callbacks = {};
     var guid = 0;
     // var ua = navigator.userAgent.toLowerCase();
-    // var ANDROID = /android/.test(ua);
-    // var IOS = /iphone|ipad/.test(ua);
-    // var WP = /windows phone/.test(ua);
+    var ua = 'nativeName/1.2 IOS10 iPhone'.toLowerCase();
+    
+    var ANDROID = /android/.test(ua);
+    var IOS = /iphone|ipad/.test(ua);
+    var WP = /windows phone/.test(ua);
 
 
-    /*添加对IOS10的兼容处理 by qp*/
-    /*
-    * IOS的UA规则如: zbj/9.3.2/zbj_ios_5.1.6_buyer
-    * 9.3.2表示IOS系统版本  5.1.6表示APP版本
-    *
-    * Android的UA规则如: zbj_android_4.1.8_seller
-    *                      4.1.8表示APP版本
-    *
-    * UA最后添加的关键字buyer(买家版) seller(钉耙)
-    *
-    * 买家版5.1.7版本才添加关键字buyer 并同时支持IOS10新协议
-    * 钉耙4.1.8版本才添加关键字seller 并同时支持IOS10新协议
-    */
-    // var BUYER = /buyer/.test(ua); // 是否为支持IOS10新协议的买家版
-    // var SELLER = /seller/.test(ua); // 是否为支持IOS10新协议的钉耙
-
-    // IOS10以下以及旧版本的客户端走oldRule
-    // function isOldRule(IOS_SYSTEM_VERSION){
-    //     // 添加了客户端区分字段才支持IOS10
-    //     var isOldVersion = !BUYER && !SELLER ;
-    //     return (IOS_SYSTEM_VERSION < 10) && isOldVersion;
-    // }
-
+    var newRule = /IOS10/.test(ua); // 是否为支持IOS10新协议的，Native端可以通过添加UA来告诉前端是否需要支持IOS10
 
     /**
      * 方便在各个平台中看到完整的 log
@@ -49,19 +21,18 @@
         }
     }
 
-
     /**
      * 平台相关的 Web 与 Native 单向通信方法
      */
     function invoke(cmd) {
         log('invoke', cmd);
 
-        // if (ANDROID) {
-            // prompt(cmd);
-        // }
-        // else if (IOS) {
-            // var IOS_SYSTEM_VERSION = /zbj\/(\d+)/.exec(ua) ? /zbj\/(\d+)/.exec(ua)[1] : 0;
-            // var bridgeRFC = isOldRule(IOS_SYSTEM_VERSION) ? 'bridge://' : 'bridge://MobileNative?query=';
+        if (ANDROID) {
+            prompt(cmd);   //android 了解 是否单纯捕获窗体
+        } else if (IOS) {
+            // 获取Native版本号 例如：nativeName/1.2 自动匹配1
+            var IOS_SYSTEM_VERSION = /nativeName\/(\d+)/.exec(ua) ? /nativeName\/(\d+)/.exec(ua)[1] : 0;
+            var bridgeRFC = IOS_SYSTEM_VERSION < 10 && !newRule ? 'bridge://' : 'bridge://MobileNative?query=';
             var bridgeRFC = 'bridge://MobileNative?query=';
 
             var iframe = document.createElement('IFRAME');
@@ -69,10 +40,9 @@
             document.documentElement.appendChild(iframe);
             iframe.parentNode.removeChild(iframe);
             iframe = null;
-        // }
-        // else if (WP) {
-        //     // ...
-        // }
+        } else if (WP) {
+            // ...
+        }
     }
 
     var Bridge = {
