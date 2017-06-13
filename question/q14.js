bcc5ac416f6f43fcd8250b2da497add462dcec85
perezyuan <yzmspirit@sina.cn>
2017-06-13 17:41:01 +0800

    [+]second mehod

--- question/q14.js
+++ question/q14.js
@@ -1,5 +1,5 @@
 var arr = [
-    {name: '小米1', value: 1, type: 2, date: '2017-06-07T08:00:01.589Z' },
+    { name: '小米1', value: 1, type: 2, date: '2017-06-07T08:00:01.589Z' },
     { name: '锤子T1', value: 1, type: 2, date: '2017-06-07T08:10:01.589Z' },
     { name: '小米2', value: 1, type: 4, date: '2017-06-07T08:00:01.589Z' },
     { name: '小米2', value: 4, type: 4, date: '2017-06-07T08:10:21.189Z' },
@@ -27,41 +27,90 @@ var arr = [
 * 最后按 value 降序(从大到小)排序.
 * 然后每行按照 "小米2,4,2017年06月07日" 的格式, 打印出来.
 */
-function printArray(arr) {
-    function isType4(obj) {
-        return obj.type === 4;
+function formatDate(gmt) {
+    var date = new Date(gmt);
+    return date.getFullYear() + '年' +
+        (date.getMonth() + 1) + '月' +
+        date.getDate() + '日';
+}
+
+function printArray2(arr) {
+    var obj = {
+        resultArr: []
     }
-    function formatDate(gmt) {
-        var date = new Date(gmt);
-        return date.getFullYear() + '年' +
-            (date.getMonth() + 1) + '月' +
-            date.getDate() + '日';
-    }
+    Object.defineProperties(obj, {
+        'item': {
+            set: function (item) {
+                if (item.type !== 4) {
+                    return;
+                }
+                this.__tempMap = this.__tempMap || {};
+                var key = item.name,
+                    date = formatDate(item.date);
+                if (this.__tempMap[key] && this.__tempMap[key].date === date) {
+                    this.__tempMap[key].value += item.value;
+                } else {
+                    this.__tempMap[key] = {
+                        name: key,
+                        value: item.value,
+                        date: date
+                    };
+                    this.resultArr.push(this.__tempMap[key]);
+                }
+            },
+            get: function () {
+                return this.str;
+            }
+        },
+        "str": {
+            set: function(newVal) {
+                return this.str = newVal;
+            },
+            get: function() {
+                return this.resultArr
+                    .sort(function (a, b) {
+                        return b.value - a.value;
+                    })
+            }
+        }
+    })
+
+    arr.forEach(function (element) {
+        obj.item = element;
+    }, this);
+    obj.item.forEach(element =>
+        console.log(element.name + ',' + element.value + ',' + element.date)
+    )
+}
+
+function printArray1(arr) {
     var resultArr = [];
     if (Array.isArray(arr)) {
-        var arr = arr.filter(isType4);
         var resultArr = [];
         var tempMap = {};
         for (var i = 0; i < arr.length; i++) {
-            var key = arr[i].name;
-            if (!tempMap[key]) {
-                tempMap[key] = {
-                    name: key,
-                    value: arr[i].value,
-                    date: arr[i].date
-                };
-                resultArr.push(tempMap[key])
-            } else {
-                tempMap[key].value += arr[i].value;
+            if (arr[i].type == 4) {
+                var key = arr[i].name,
+                    date = formatDate(arr[i].date);
+                if (tempMap[key] && tempMap[key].date === date) {
+                    tempMap[key].value += arr[i].value;
+                } else {
+                    tempMap[key] = {
+                        name: key,
+                        value: arr[i].value,
+                        date: date
+                    };
+                    resultArr.push(tempMap[key]);
+                }
             }
         }
         resultArr
-            .sort(function(a, b) {
+            .sort(function (a, b) {
                 return b.value - a.value;
             })
-            .map(x => x.name + ',' + x.value + ',' + formatDate(x.date))
-            .forEach(x => console.log(x));
+            .forEach(element => console.log(element.name + ',' + element.value + ',' + element.date));
     }
 }
-
-printArray(arr);
+printArray1(arr);
+console.log('________')
+printArray2(arr);
