869baa55fc26d6a5a0bdbd442c7ada832d339371
perezyuan <yzmspirit@sina.cn>
2017-06-15 16:48:48 +0800

    [+]Generator异步编程原理

--- AJAX/public/javascripts/ES6-generator-asyn.js
+++ AJAX/public/javascripts/ES6-generator-asyn.js
@@ -1,9 +1,22 @@
 document.getElementById('clickBtn').onclick = function () {
-    var it = longRunningTask("first invoke");
-    console.log(it.next());
-    console.log(it.next());
+    var it = myLogic('called');
+    // 本质上是个递归而已，判断generator的done就可以封装， TJ大神的Co
+    it.next().value.then(function (result1) {
+        it.next(result1).value.then(function(result2) {
+            it.next(result2);
+        });
+    });
 }
 
+// Generator函数看起来就像同步函数一样
+function* myLogic(value) {
+    var serverData = yield myAjax(value);
+    console.log(serverData);
+    var serverData2 = yield myAjax(serverData.req + " again")
+    console.log(serverData2);
+}
+
+// 以下公共代码
 function createXHR() {
     if (typeof XMLHttpRequest != 'undefined') {
         return new XMLHttpRequest();
@@ -12,21 +25,16 @@ function createXHR() {
     }
 }
 
-function sendXHR(arg) {
-    var xhr = createXHR();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status >=200 && xhr.status < 300 || xhr.status == 304) {
-            return(JSON.parse(xhr.responseText));
-        } else {
-            console.log(xhr.status);
+function myAjax(value) {
+    return new Promise(function (resolve, reject) {
+        var xhr = createXHR();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status >= 200 && xhr.status < 300 || xhr.status == 304) {
+                resolve(JSON.parse(xhr.responseText));
+            }
         }
-    }
-    var url = "/getjson?a=" + arg;
-    xhr.open("get", url, true);
-    xhr.send(null);
-}
-
-function* longRunningTask(value) {
-    var value2 = yield sendXHR(value);
-    var value3 = yield sendXHR(value2 + "next");
+        var url = "/getjson?a=" + value;
+        xhr.open("get", url, true);
+        xhr.send(null);
+    })
 }
