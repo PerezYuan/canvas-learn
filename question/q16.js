32271460933c35a253b640b0801ccce5205577e8
perezyuan <yzmspirit@sina.cn>
2017-06-30 22:11:26 +0800

    [+] hex to rgb

---
+++ question/q16.js
@@ -0,0 +1,17 @@
+function hexToRgb(hex) {
+    // 扩展#03f => #0033ff
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+    // 核心分层
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+console.log( hexToRgb("#0033ff").g ); // "51";
+console.log( hexToRgb("#03f").g ); // "51";
